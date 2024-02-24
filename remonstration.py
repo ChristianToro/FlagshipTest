4d488ff65a5c3d32e5bafb383b076daa1b69f4a2
ChristianToro <143457701+ChristianToro@users.noreply.github.com>
2024-02-23 19:41:04 -0500

    Update remonstration.py

--- remonstration.py
+++ remonstration.py
@@ -1,1 +1,18 @@
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = 'shakespeare.txt'
+fh = open(fname)
 
+word_freq = {}
+
+for line in fh:
+  words = line.split()
+  for word in words:
+    if word in word_freq:
+      word_freq[word] += 1
+    else:
+      word_freq[word] = 1
+
+
+for word, freq in word_freq.items():
+  print(word, freq)
